d2d4f4fadc6d4a29b969945bb64b28e710a4e996
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-25 23:36:21 +0900

    add init to HomeGroupCellReactor

--- goyotashi/UI/Organisms/Home/HomeGroupCellReactor.swift
+++ goyotashi/UI/Organisms/Home/HomeGroupCellReactor.swift
@@ -12,13 +12,21 @@ final class HomeGroupCellReactor: Reactor {
     enum Mutation {}
 
     struct State {
-        let homeGroup: HomeGroup = TestData.homeGroup()
+        let homeGroup: HomeGroup
         var description: String {
             return "\(homeGroup.restaurantCount)件のお店 / \(homeGroup.memberCount)人のメンバー"
         }
+
+        init(homeGroup: HomeGroup) {
+            self.homeGroup = homeGroup
+        }
     }
 
-    let initialState: State = State()
+    let initialState: State
+
+    init(homeGroup: HomeGroup) {
+        initialState = State(homeGroup: homeGroup)
+    }
 }
 
 extension HomeGroupCellReactor: Equatable {
