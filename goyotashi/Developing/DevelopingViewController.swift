a0df264bb53f4dc14f4515ee3854472d44f4aeab
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-25 15:04:45 +0900

    update code format

--- goyotashi/Developing/DevelopingViewController.swift
+++ goyotashi/Developing/DevelopingViewController.swift
@@ -14,18 +14,18 @@ final class DevelopingViewController: UIViewController, ViewConstructor {
         $0.text = "Developing ViewController"
         $0.textAlignment = .center
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         setupViews()
         setupViewConstraints()
     }
-    
+
     func setupViews() {
         view.addSubview(developingLabel)
     }
-    
+
     func setupViewConstraints() {
         developingLabel.snp.makeConstraints {
             $0.center.equalToSuperview()
--- goyotashi/Extensions/UIColor+Extension.swift
+++ goyotashi/Extensions/UIColor+Extension.swift
@@ -9,11 +9,11 @@ import UIKit
 
 extension UIColor {
     convenience init(hex: String, alpha: CGFloat) {
-        let v = hex.map { String($0) } + Array(repeating: "0", count: max(6 - hex.count, 0))
-        let r = CGFloat(Int(v[0] + v[1], radix: 16) ?? 0) / 255.0
-        let g = CGFloat(Int(v[2] + v[3], radix: 16) ?? 0) / 255.0
-        let b = CGFloat(Int(v[4] + v[5], radix: 16) ?? 0) / 255.0
-        self.init(red: r, green: g, blue: b, alpha: min(max(alpha, 0), 1))
+        let value = hex.map { String($0) } + Array(repeating: "0", count: max(6 - hex.count, 0))
+        let red = CGFloat(Int(value[0] + value[1], radix: 16) ?? 0) / 255.0
+        let green = CGFloat(Int(value[2] + value[3], radix: 16) ?? 0) / 255.0
+        let blue = CGFloat(Int(value[4] + value[5], radix: 16) ?? 0) / 255.0
+        self.init(red: red, green: green, blue: blue, alpha: min(max(alpha, 0), 1))
     }
 
     convenience init(hex: String) {
