7934062cbb3cc086e8c01ba5a9780a217bb4219d
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-25 14:28:50 +0900

    add Color to Atoms

---
+++ goyotashi/UI/Atoms/Color.swift
@@ -0,0 +1,40 @@
+//
+//  Color.swift
+//  goyotashi
+//
+//  Created by Akihiro Kokubo on 2021/10/25.
+//
+
+import UIKit
+
+struct Color {
+    private init() {}
+
+    static var white: UIColor {
+        return UIColor(hex: "FFFFFF")
+    }
+
+    static var gray01: UIColor {
+        return UIColor(hex: "333333")
+    }
+
+    static var gray02: UIColor {
+        return UIColor(hex: "4F4F4F")
+    }
+
+    static var gray03: UIColor {
+        return UIColor(hex: "828282")
+    }
+
+    static var gray04: UIColor {
+        return UIColor(hex: "BDBDBD")
+    }
+
+    static var gray05: UIColor {
+        return UIColor(hex: "E0E0E0")
+    }
+
+    static var gray06: UIColor {
+        return UIColor(hex: "F2F2F2")
+    }
+}
