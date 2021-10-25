b198ead3985f6d91034965bc9180005235ea03bc
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-26 05:13:30 +0900

    add ProfileGroup

---
+++ goyotashi/BusinessLogic/Entities/ProfileGroup.swift
@@ -0,0 +1,14 @@
+//
+//  ProfileGroup.swift
+//  goyotashi
+//
+//  Created by Akihiro Kokubo on 2021/10/26.
+//
+
+struct ProfileGroup {
+    let groupId: String
+    let groupName: String
+    let restaurantCount: Int
+    let memberCount: Int
+    let imageUrls: [String]
+}
