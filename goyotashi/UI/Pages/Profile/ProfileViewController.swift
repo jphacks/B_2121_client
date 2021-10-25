5f9252b25a6337eb2c0cfe0e8f320264102b331f
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-26 01:44:29 +0900

    add ProfileViewController and ProfileReactor

---
+++ goyotashi/UI/Pages/Profile/ProfileViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ProfileViewController.swift
+//  goyotashi
+//
+//  Created by Akihiro Kokubo on 2021/10/26.
+//
+
+import UIKit
+import ReactorKit
+
+final class ProfileViewController: UIViewController, View, ViewConstructor {
+
+    // MARK: - Variables
+    var disposeBag = DisposeBag()
+
+    // MARK: - Views
+
+    // MARK: - Lify Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupViews()
+        setupViewConstraints()
+    }
+
+    // MARK: - Setup Methods
+    func setupViews() {
+
+    }
+
+    func setupViewConstraints() {
+
+    }
+
+    // MARK: - Bind Method
+    func bind(reactor: ProfileReactor) {
+        // Action
+
+        // State
+    }
+}
