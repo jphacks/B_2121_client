af42007d5bbde2caa9b22c488580030b081e83ae
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-26 04:03:08 +0900

    add collectionView to ProfileGroupListViewController

--- goyotashi/UI/Pages/Profile/ProfileGroupList/ProfileGroupListViewController.swift
+++ goyotashi/UI/Pages/Profile/ProfileGroupList/ProfileGroupListViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 import ReactorKit
+import ReusableKit
 
 final class ProfileGroupListViewController: UIViewController, View, ViewConstructor {
+
+    struct Reusable {
+        static let groupCell = ReusableCell<ProfileGroupListCell>()
+    }
 
     // MARK: - Variables
     var disposeBag = DisposeBag()
 
     // MARK: - Views
+    private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout().then {
+        $0.estimatedItemSize =  ProfileGroupListCell.Const.itemSize
+        $0.minimumLineSpacing = 32
+        $0.scrollDirection = .vertical
+    }).then {
+        $0.register(Reusable.groupCell)
+        $0.contentInset = UIEdgeInsets(top: 0, left: 16, bottom: 56, right: 16)
+        $0.backgroundColor = Color.white
+        $0.alwaysBounceVertical = true
+    }
 
     // MARK: - Lify Cycles
     override func viewDidLoad() {
@@ -25,11 +40,13 @@ final class ProfileGroupListViewController: UIViewController, View, ViewConstruc
 
     // MARK: - Setup Methods
     func setupViews() {
-
+        view.addSubview(collectionView)
     }
 
     func setupViewConstraints() {
-
+        collectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
 
     // MARK: - Bind Method
@@ -37,5 +54,11 @@ final class ProfileGroupListViewController: UIViewController, View, ViewConstruc
         // Action
 
         // State
+        reactor.state.map { $0.groupCellReactors }
+            .distinctUntilChanged()
+            .bind(to: collectionView.rx.items(Reusable.groupCell)) { _, reactor, cell in
+                cell.reactor = reactor
+            }
+            .disposed(by: disposeBag)
     }
 }
