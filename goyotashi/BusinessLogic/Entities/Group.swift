baaa93a1d85408608c9364f28d8b10720629193c
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-26 16:41:37 +0900

    add Group

---
+++ goyotashi/BusinessLogic/Entities/Group.swift
@@ -0,0 +1,14 @@
+//
+//  Group.swift
+//  goyotashi
+//
+//  Created by Akihiro Kokubo on 2021/10/26.
+//
+
+struct Group {
+    let id: String
+    let name: String
+    let description: String
+    let memberCount: Int
+    let restaurantCount: Int
+}
