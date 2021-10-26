579f5d6d4cfafea873a60ee4f805d7afd13dc7e6
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-27 01:18:17 +0900

    add OrganizeRestaurantViewController and OrganizeRestaurantReactor

---
+++ goyotashi/UI/Pages/OrganizeRestaurant/OrganizeRestaurantReactor.swift
@@ -0,0 +1,19 @@
+//
+//  OrganizeRestaurantReactor.swift
+//  goyotashi
+//
+//  Created by Akihiro Kokubo on 2021/10/27.
+//
+
+import ReactorKit
+
+final class OrganizeRestaurantReactor: Reactor {
+    enum Action {}
+    enum Mutation {}
+
+    struct State {
+        let property: Int = 0
+    }
+
+    let initialState: State = State()
+}
