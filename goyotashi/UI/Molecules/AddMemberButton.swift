1ede1ed0dd99a32ec0249d068bb7e9daa20c130f
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-27 01:00:32 +0900

    add overlayView

--- goyotashi/UI/Molecules/AddMemberButton.swift
+++ goyotashi/UI/Molecules/AddMemberButton.swift
@@ -15,6 +15,7 @@ final class AddMemberButton: UIButton {
 
     override var isHighlighted: Bool {
         didSet {
+            overlayView.isHidden = !isHighlighted
         }
     }
 
@@ -22,6 +23,12 @@ final class AddMemberButton: UIButton {
     private let addMemberImageView = UIImageView().then {
         $0.contentMode = .scaleAspectFit
         $0.image = R.image.person_add()
+    }
+
+    private let overlayView = UIView().then {
+        $0.backgroundColor = Color.white.withAlphaComponent(0.8)
+        $0.isUserInteractionEnabled = false
+        $0.isHidden = true
     }
 
     override init(frame: CGRect) {
@@ -41,6 +48,7 @@ final class AddMemberButton: UIButton {
         layer.masksToBounds = true
 
         addSubview(addMemberImageView)
+        addSubview(overlayView)
     }
 
     func setupViewConstraints() {
@@ -50,6 +58,9 @@ final class AddMemberButton: UIButton {
         addMemberImageView.snp.makeConstraints {
             $0.center.equalToSuperview()
             $0.size.equalTo(Const.imageViewSize)
+        }
+        overlayView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
         }
     }
 }
