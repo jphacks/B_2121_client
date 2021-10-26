a664b51dbdc0f2d9f2341cf853b37ee4d4e6ffd4
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-26 20:26:18 +0900

    add restaurant to RestaurantReactor

--- goyotashi/UI/Pages/Restaurant/RestaurantReactor.swift
+++ goyotashi/UI/Pages/Restaurant/RestaurantReactor.swift
@@ -12,6 +12,7 @@ final class RestaurantReactor: Reactor {
     enum Mutation {}
 
     struct State {
+        let restaurant: Restaurant = TestData.restaurant()
         var groupCellReactors: [RestaurantOtherGroupCellReactor] = TestData.restaurantOtherGroups(count: 9).map { RestaurantOtherGroupCellReactor(restaurantOtherGroup: $0) }
     }
 
