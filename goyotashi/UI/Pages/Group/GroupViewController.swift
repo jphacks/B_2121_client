a50f477e7e618ec7a5d972c6f89700ae3f0ddd9c
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-26 15:32:35 +0900

    add collectionView

--- goyotashi/UI/Pages/Group/GroupViewController.swift
+++ goyotashi/UI/Pages/Group/GroupViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 import ReactorKit
+import ReusableKit
 
 final class GroupViewController: UIViewController, View, ViewConstructor {
+
+    struct Reusable {
+        static let restaurantCell = ReusableCell<GroupRestaurantCell>()
+    }
 
     // MARK: - Variables
     var disposeBag = DisposeBag()
 
     // MARK: - Views
+    private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout().then {
+        $0.estimatedItemSize =  GroupRestaurantCell.Const.itemSize
+        $0.minimumLineSpacing = 24
+        $0.minimumInteritemSpacing = 8
+        $0.scrollDirection = .vertical
+    }).then {
+        $0.register(Reusable.restaurantCell)
+        $0.contentInset = UIEdgeInsets(top: 0, left: 16, bottom: 56, right: 16)
+        $0.backgroundColor = Color.white
+        $0.alwaysBounceVertical = true
+    }
 
     // MARK: - Lify Cycles
     override func viewDidLoad() {
@@ -25,11 +41,13 @@ final class GroupViewController: UIViewController, View, ViewConstructor {
 
     // MARK: - Setup Methods
     func setupViews() {
-
+        view.addSubview(collectionView)
     }
 
     func setupViewConstraints() {
-
+        collectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
 
     // MARK: - Bind Method
