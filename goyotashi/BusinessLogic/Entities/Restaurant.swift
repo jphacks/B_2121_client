f966f02aad946a565727ed9d965090417492d16c
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-26 20:20:18 +0900

    add Restaurant

---
+++ goyotashi/BusinessLogic/Entities/Restaurant.swift
@@ -0,0 +1,16 @@
+//
+//  Restaurant.swift
+//  goyotashi
+//
+//  Created by Akihiro Kokubo on 2021/10/26.
+//
+
+struct Restaurant {
+    let id: String
+    let imageUrl: String
+    let name: String
+    let description: String
+    let address: String
+    let phoneNumber: String
+    let openingHours: String
+}
