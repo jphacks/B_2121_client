077a5496859398caac2c697623097971af576208
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-26 20:04:01 +0900

    add RestaurantOtherGroup

---
+++ goyotashi/BusinessLogic/Entities/RestaurantOtherGroup.swift
@@ -0,0 +1,14 @@
+//
+//  RestaurantOtherGroup.swift
+//  goyotashi
+//
+//  Created by Akihiro Kokubo on 2021/10/26.
+//
+
+struct RestaurantOtherGroup {
+    let groupId: String
+    let groupName: String
+    let restaurantCount: Int
+    let memberCount: Int
+    let imageUrls: [String]
+}
