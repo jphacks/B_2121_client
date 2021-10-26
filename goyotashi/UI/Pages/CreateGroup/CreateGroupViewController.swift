3ffc75496f70ae454ebba3a7c0a2e71dcccc82f2
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-26 22:41:20 +0900

    add groupNameLabel

--- goyotashi/UI/Pages/CreateGroup/CreateGroupViewController.swift
+++ goyotashi/UI/Pages/CreateGroup/CreateGroupViewController.swift
@@ -14,7 +14,14 @@ final class CreateGroupViewController: UIViewController, View, ViewConstructor {
     var disposeBag = DisposeBag()
 
     // MARK: - Views
-    private let scrollView = UIScrollView()
+    private let scrollView = UIScrollView().then {
+        $0.alwaysBounceVertical = true
+    }
+
+    private let groupNameLabel = UILabel().then {
+        $0.apply(fontStyle: .regular, size: 15, color: Color.gray01)
+        $0.text = "グループ名"
+    }
 
     // MARK: - Lify Cycles
     override func viewDidLoad() {
@@ -27,11 +34,16 @@ final class CreateGroupViewController: UIViewController, View, ViewConstructor {
     // MARK: - Setup Methods
     func setupViews() {
         view.addSubview(scrollView)
+        scrollView.addSubview(groupNameLabel)
     }
 
     func setupViewConstraints() {
         scrollView.snp.makeConstraints {
             $0.edges.equalToSuperview()
+        }
+        groupNameLabel.snp.makeConstraints {
+            $0.top.equalToSuperview().inset(24)
+            $0.left.equalToSuperview().inset(16)
         }
     }
 
