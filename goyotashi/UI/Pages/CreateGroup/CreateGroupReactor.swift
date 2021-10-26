bcf71abb868b127e4935c3a76b0b662aba109fc8
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-27 00:01:58 +0900

    add updateGroupName and setGroupName

--- goyotashi/UI/Pages/CreateGroup/CreateGroupReactor.swift
+++ goyotashi/UI/Pages/CreateGroup/CreateGroupReactor.swift
@@ -8,12 +8,33 @@
 import ReactorKit
 
 final class CreateGroupReactor: Reactor {
-    enum Action {}
-    enum Mutation {}
+    enum Action {
+        case updateGroupName(String?)
+    }
+    enum Mutation {
+        case setGroupName(String?)
+    }
 
     struct State {
+        var groupName: String?
         let members: [User] = (0 ..< 4).map { _ in TestData.user() }
     }
 
     let initialState: State = State()
+
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case let .updateGroupName(name):
+            return .just(Mutation.setGroupName(name))
+        }
+    }
+
+    func reduce(state: State, mutation: Mutation) -> State {
+        var state = state
+        switch mutation {
+        case let .setGroupName(name):
+            state.groupName = name
+        }
+        return state
+    }
 }
