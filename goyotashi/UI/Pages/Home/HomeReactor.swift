22d9b1872b3714560479b974235a0804f9598f8b
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-27 21:19:00 +0900

    remove cellReactors from HomeReactor

--- goyotashi/UI/Pages/Home/HomeReactor.swift
+++ goyotashi/UI/Pages/Home/HomeReactor.swift
@@ -8,38 +8,10 @@
 import ReactorKit
 
 final class HomeReactor: Reactor {
-    enum Action {
-        case refresh
-    }
-    enum Mutation {
-        case setGroupCellReactors([HomeGroup])
-    }
+    enum Action {}
+    enum Mutation {}
 
-    struct State {
-        var groupCellReactors: [HomeGroupCellReactor] = []
-    }
+    struct State {}
 
     let initialState: State = State()
-
-    func mutate(action: Action) -> Observable<Mutation> {
-        switch action {
-        case .refresh:
-            let homeGroups = TestData.homeGroups(count: 6)
-            return .just(Mutation.setGroupCellReactors(homeGroups))
-        }
-    }
-
-    func reduce(state: State, mutation: Mutation) -> State {
-        var state = state
-        switch mutation {
-        case let .setGroupCellReactors(homeGroups):
-            state.groupCellReactors = homeGroups.map { HomeGroupCellReactor(homeGroup: $0) }
-        }
-        return state
-    }
-
-    // MARK: Create Reactor Methods
-    func createGroupReactor(indexPath: IndexPath) -> GroupReactor {
-        return GroupReactor()
-    }
 }
