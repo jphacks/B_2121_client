186e212e37238e3c2888907c9a7010be4501f73e
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-27 17:55:48 +0900

    add collectionView

--- goyotashi/UI/Pages/AddRestaurantToGroup/AddRestaurantToGroupViewController.swift
+++ goyotashi/UI/Pages/AddRestaurantToGroup/AddRestaurantToGroupViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 import ReactorKit
+import ReusableKit
 
 final class AddRestaurantToGroupViewController: UIViewController, View, ViewConstructor {
+
+    struct Reusable {
+        static let groupCell = ReusableCell<AddRestaurantToGroupCell>()
+    }
 
     // MARK: - Variables
     var disposeBag = DisposeBag()
 
     // MARK: - Views
+    private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout().then {
+        $0.itemSize =  AddRestaurantToGroupCell.Const.itemSize
+        $0.minimumLineSpacing = 16
+        $0.scrollDirection = .vertical
+    }).then {
+        $0.register(Reusable.groupCell)
+        $0.contentInset = UIEdgeInsets(top: 0, left: 16, bottom: 56, right: 16)
+        $0.backgroundColor = Color.white
+        $0.alwaysBounceVertical = true
+    }
 
     // MARK: - Lify Cycles
     override func viewDidLoad() {
@@ -25,11 +40,13 @@ final class AddRestaurantToGroupViewController: UIViewController, View, ViewCons
 
     // MARK: - Setup Methods
     func setupViews() {
-
+        view.addSubview(collectionView)
     }
 
     func setupViewConstraints() {
-
+        collectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
 
     // MARK: - Bind Method
@@ -37,5 +54,11 @@ final class AddRestaurantToGroupViewController: UIViewController, View, ViewCons
         // Action
 
         // State
+        reactor.state.map { $0.groupCellReactors }
+            .distinctUntilChanged()
+            .bind(to: collectionView.rx.items(Reusable.groupCell)) { _, reactor, cell in
+                cell.reactor = reactor
+            }
+            .disposed(by: disposeBag)
     }
 }
