f442ee926036d89d8ab2ccf232891595c70a97f9
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-27 13:18:01 +0900

    add collectionView

--- goyotashi/UI/Pages/InviteMember/InviteMemberViewController.swift
+++ goyotashi/UI/Pages/InviteMember/InviteMemberViewController.swift
@@ -7,13 +7,28 @@
 
 import UIKit
 import ReactorKit
+import ReusableKit
 
 final class InviteMemberViewController: UIViewController, View, ViewConstructor {
+
+    struct Reusable {
+        static let memberCell = ReusableCell<InviteMemberCell>()
+    }
 
     // MARK: - Variables
     var disposeBag = DisposeBag()
 
     // MARK: - Views
+    private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout().then {
+        $0.estimatedItemSize =  InviteMemberCell.Const.itemSize
+        $0.minimumLineSpacing = 0
+        $0.scrollDirection = .vertical
+    }).then {
+        $0.register(Reusable.memberCell)
+        $0.contentInset = UIEdgeInsets(top: 0, left: 0, bottom: 56, right: 0)
+        $0.backgroundColor = Color.white
+        $0.alwaysBounceVertical = true
+    }
 
     // MARK: - Lify Cycles
     override func viewDidLoad() {
@@ -25,11 +40,13 @@ final class InviteMemberViewController: UIViewController, View, ViewConstructor 
 
     // MARK: - Setup Methods
     func setupViews() {
-
+        view.addSubview(collectionView)
     }
 
     func setupViewConstraints() {
-
+        collectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
 
     // MARK: - Bind Method
@@ -37,5 +54,11 @@ final class InviteMemberViewController: UIViewController, View, ViewConstructor 
         // Action
 
         // State
+        reactor.state.map { $0.memberCellReactors }
+            .distinctUntilChanged()
+            .bind(to: collectionView.rx.items(Reusable.memberCell)) { _, reactor, cell in
+                cell.reactor = reactor
+            }
+            .disposed(by: disposeBag)
     }
 }
