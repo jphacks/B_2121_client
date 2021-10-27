56125eff8ce7b9f8791c09e5fbc31a3791b2b416
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-27 22:00:32 +0900

    add search to SearchGroupResultReactor

--- goyotashi/UI/Pages/Home/SearchGroupResult/SearchGroupResultReactor.swift
+++ goyotashi/UI/Pages/Home/SearchGroupResult/SearchGroupResultReactor.swift
@@ -8,12 +8,37 @@
 import ReactorKit
 
 final class SearchGroupResultReactor: Reactor {
-    enum Action {}
-    enum Mutation {}
+    enum Action {
+        case search(String)
+    }
+    enum Mutation {
+        case setGroupCellReactors([HomeGroup])
+    }
 
     struct State {
         var groupCellReactors: [HomeGroupCellReactor] = TestData.homeGroups(count: 8).map { HomeGroupCellReactor(homeGroup: $0) }
     }
 
     let initialState: State = State()
+
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case let .search(keyword):
+            return search(keyword: keyword)
+                .map(Mutation.setGroupCellReactors)
+        }
+    }
+
+    private func search(keyword: String) -> Observable<[HomeGroup]> {
+        return .just(TestData.homeGroups(count: Int.random(in: 0 ..< 7)))
+    }
+
+    func reduce(state: State, mutation: Mutation) -> State {
+        var state = state
+        switch mutation {
+        case let .setGroupCellReactors(homeGroups):
+            state.groupCellReactors = homeGroups.map { HomeGroupCellReactor(homeGroup: $0) }
+        }
+        return state
+    }
 }
