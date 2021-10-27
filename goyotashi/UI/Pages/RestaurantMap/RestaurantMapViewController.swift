6a7b6c2fe09957a537df60b783cf77a1c3c5b14d
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-27 22:52:43 +0900

    add RestaurantMapViewController

---
+++ goyotashi/UI/Pages/RestaurantMap/RestaurantMapViewController.swift
@@ -0,0 +1,57 @@
+//
+//  RestaurantMapViewController.swift
+//  goyotashi
+//
+//  Created by Akihiro Kokubo on 2021/10/27.
+//
+
+import UIKit
+import MapKit
+
+class RestaurantMapViewController: UIViewController, ViewConstructor {
+
+    private let mapView = MKMapView()
+    private let location: Location
+    private let placeName: String
+
+    init(placeName: String, location: Location) {
+        self.location = location
+        self.placeName = placeName
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupViews()
+        setupViewConstraints()
+        setMap(location: location)
+    }
+
+    func setupViews() {
+        title = placeName
+        view.addSubview(mapView)
+    }
+
+    func setupViewConstraints() {
+        mapView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
+    }
+
+    private func setMap(location: Location) {
+        let center: CLLocationCoordinate2D = CLLocationCoordinate2D(latitude: CLLocationDegrees(location.latitude), longitude: CLLocationDegrees(location.longitude))
+        mapView.setCenter(center, animated: false)
+        let span: MKCoordinateSpan = MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        let region: MKCoordinateRegion = MKCoordinateRegion(center: center, span: span)
+        mapView.region = region
+        let pointAnnotation: MKPointAnnotation = MKPointAnnotation()
+        pointAnnotation.coordinate = center
+        pointAnnotation.title = placeName
+        mapView.addAnnotation(pointAnnotation)
+    }
+}
