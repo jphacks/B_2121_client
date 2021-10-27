ae166147de6d66df1f0245a62001c36697928249
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-27 13:47:44 +0900

    add closeButton

--- goyotashi/UI/Pages/InviteMember/InviteMemberViewController.swift
+++ goyotashi/UI/Pages/InviteMember/InviteMemberViewController.swift
@@ -19,6 +19,10 @@ final class InviteMemberViewController: UIViewController, View, ViewConstructor 
     var disposeBag = DisposeBag()
 
     // MARK: - Views
+    private let closeButton = UIButton().then {
+        $0.setImage(R.image.close(), for: .normal)
+    }
+
     private let doneButton = UIBarButtonItem(title: "完了", style: .done, target: nil, action: nil).then {
         $0.tintColor = Color.gray01
     }
@@ -61,6 +65,7 @@ final class InviteMemberViewController: UIViewController, View, ViewConstructor 
     func setupViews() {
         title = "メンバーを招待"
         view.backgroundColor = Color.white
+        navigationItem.leftBarButtonItem = UIBarButtonItem(customView: closeButton)
         navigationItem.rightBarButtonItem = doneButton
 
         view.addSubview(searchBar)
