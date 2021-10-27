9e1b3309f0e6c063e5a150ca982d66b93dc446a3
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-27 16:48:46 +0900

    add search

--- goyotashi/UI/Pages/SearchRestaurant/SearchRestaurantResult/SearchRestaurantResultReactor.swift
+++ goyotashi/UI/Pages/SearchRestaurant/SearchRestaurantResult/SearchRestaurantResultReactor.swift
@@ -8,12 +8,32 @@
 import ReactorKit
 
 final class SearchRestaurantResultReactor: Reactor {
-    enum Action {}
-    enum Mutation {}
+    enum Action {
+        case search(String)
+    }
+    enum Mutation {
+        case setRestaurantCellReactors([Restaurant])
+    }
 
     struct State {
-        let restaurantCellReactors: [SearchRestaurantCellReactor] = (0 ..< 9).map { _ in SearchRestaurantCellReactor() }
+        var restaurantCellReactors: [SearchRestaurantCellReactor] = (0 ..< 9).map { _ in SearchRestaurantCellReactor() }
     }
 
     let initialState: State = State()
+
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case let .search(keyword):
+            return .empty()
+        }
+    }
+
+    func reduce(state: State, mutation: Mutation) -> State {
+        var state = state
+        switch mutation {
+        case let .setRestaurantCellReactors(restaurants):
+            state.restaurantCellReactors = restaurants.map { _ in SearchRestaurantCellReactor() }
+        }
+        return state
+    }
 }
