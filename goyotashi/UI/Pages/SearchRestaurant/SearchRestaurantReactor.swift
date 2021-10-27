275bf8c81ccc1a48d94111e1ebbc61935a9f241c
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-27 16:09:24 +0900

    add keyword to reactor

--- goyotashi/UI/Pages/SearchRestaurant/SearchRestaurantReactor.swift
+++ goyotashi/UI/Pages/SearchRestaurant/SearchRestaurantReactor.swift
@@ -12,7 +12,7 @@ final class SearchRestaurantReactor: Reactor {
     enum Mutation {}
 
     struct State {
-        let property: Int = 0
+        var keyword: String = ""
     }
 
     let initialState: State = State()
