11dc6bd802f2a3cfe860f310ad20f78d64afa937
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-28 05:17:01 +0900

    add UserService

---
+++ goyotashi/BusinessLogic/Services/UserService.swift
@@ -0,0 +1,10 @@
+//
+//  UserService.swift
+//  goyotashi
+//
+//  Created by Akihiro Kokubo on 2021/10/28.
+//
+
+protocol UserServiceType {}
+
+final class UserService: BaseService, UserServiceType {}
