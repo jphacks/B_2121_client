074df370cf29f1a32e2fe3e05bfb1f7a89d86b0e
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-27 20:59:56 +0900

    add collectionView

--- goyotashi/UI/Pages/Home/SearchGroupResult/SearchGroupResultViewController.swift
+++ goyotashi/UI/Pages/Home/SearchGroupResult/SearchGroupResultViewController.swift
@@ -7,13 +7,29 @@
 
 import UIKit
 import ReactorKit
+import ReusableKit
 
 final class SearchGroupResultViewController: UIViewController, View, ViewConstructor {
+
+    struct Reusable {
+        static let groupCell = ReusableCell<HomeGroupCell>()
+    }
 
     // MARK: - Variables
     var disposeBag = DisposeBag()
 
     // MARK: - Views
+    private let collectionView = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout().then {
+        $0.itemSize =  HomeGroupCell.Const.itemSize
+        $0.minimumLineSpacing = 32
+        $0.scrollDirection = .vertical
+        $0.sectionInset.top = HomeHeaderView.Const.height + 32
+    }).then {
+        $0.register(Reusable.groupCell)
+        $0.contentInset = UIEdgeInsets(top: 0, left: 16, bottom: 56, right: 16)
+        $0.backgroundColor = Color.white
+        $0.alwaysBounceVertical = true
+    }
 
     // MARK: - Lify Cycles
     override func viewDidLoad() {
@@ -25,11 +41,13 @@ final class SearchGroupResultViewController: UIViewController, View, ViewConstru
 
     // MARK: - Setup Methods
     func setupViews() {
-
+        view.addSubview(collectionView)
     }
 
     func setupViewConstraints() {
-
+        collectionView.snp.makeConstraints {
+            $0.edges.equalToSuperview()
+        }
     }
 
     // MARK: - Bind Method
