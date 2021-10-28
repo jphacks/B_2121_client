e91aaeef4b362a866764d16324c8ce788b71c066
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-28 14:51:42 +0900

    add getOtherGroups to RestaurantServiceType

--- goyotashi/BusinessLogic/Services/RestaurantService.swift
+++ goyotashi/BusinessLogic/Services/RestaurantService.swift
@@ -13,6 +13,7 @@ protocol RestaurantServiceType {
     func removeRestaurantFromGroup(restaurantId: String, groupId: String) -> Single<Void>
     func searchRestaurants(keyword: String, location: Location?) -> Single<[Restaurant]>
     func getRestaurant(restaurantId: String) -> Single<Restaurant>
+    func getOtherGroups(restaurantId: String) -> Single<[RestaurantOtherGroup]>
 }
 
 final class RestaurantService: BaseService, RestaurantServiceType {
@@ -34,5 +35,9 @@ final class RestaurantService: BaseService, RestaurantServiceType {
 
     func getRestaurant(restaurantId: String) -> Single<Restaurant> {
         return .just(TestData.restaurant())
+    }
+
+    func getOtherGroups(restaurantId: String) -> Single<[RestaurantOtherGroup]> {
+        return .just(TestData.restaurantOtherGroups(count: 5))
     }
 }
