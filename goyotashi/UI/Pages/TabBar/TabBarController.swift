eec02b2dfc505ba5f9af2de89e4e244e9edb43f7
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-28 13:01:27 +0900

    add provider to TabBarController

--- goyotashi/UI/Pages/TabBar/TabBarController.swift
+++ goyotashi/UI/Pages/TabBar/TabBarController.swift
@@ -14,6 +14,18 @@ final class TabBarController: UITabBarController {
         static let tabBarSelectedImages: [UIImage?] = [R.image.home_filled(), R.image.user_filled()]
     }
 
+    private let provider: ServiceProviderType
+
+    // MARK: - Initializers
+    init(provider: ServiceProviderType) {
+        self.provider = provider
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
     // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
