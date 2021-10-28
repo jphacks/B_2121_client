abf99d8277628f9fb57e39f5a813cdff3173a2cc
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-28 13:09:16 +0900

    add createProfileGroupListReactor

--- goyotashi/UI/Pages/Profile/ProfileReactor.swift
+++ goyotashi/UI/Pages/Profile/ProfileReactor.swift
@@ -24,6 +24,10 @@ final class ProfileReactor: Reactor {
     }
 
     // MARK: - Create Reactor Methods
+    func createProfileGroupListReactor() -> ProfileGroupListReactor {
+        return ProfileGroupListReactor()
+    }
+
     func createCreateGroupReactor() -> CreateGroupReactor {
         return CreateGroupReactor()
     }
