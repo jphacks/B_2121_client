f9ca9f0f097fad1cac13dba4225a9fa2276102c8
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-29 01:57:28 +0900

    add DefaultsKeys

---
+++ goyotashi/BusinessLogic/Stores/DefaultKeys.swift
@@ -0,0 +1,13 @@
+//
+//  DefaultKeys.swift
+//  goyotashi
+//
+//  Created by Akihiro Kokubo on 2021/10/29.
+//
+
+import SwiftyUserDefaults
+
+extension DefaultsKeys {
+    // MARK: - Auth
+    var authInfo: DefaultsKey<[AuthInfo?]> { return .init("authInfo", defaultValue: nil) }
+}
