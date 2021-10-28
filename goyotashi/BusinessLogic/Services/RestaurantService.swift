ae42a1ed5f51266b5f2ff5356eb212e8a26e7212
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-28 11:39:29 +0900

    add searchRestaurants

--- goyotashi/BusinessLogic/Services/RestaurantService.swift
+++ goyotashi/BusinessLogic/Services/RestaurantService.swift
@@ -11,6 +11,7 @@ protocol RestaurantServiceType {
     func getRestaurants(groupId: String) -> Single<[GroupRestaurant]>
     func addRestaurantToGroup(restaurantId: String, groupId: String) -> Single<Void>
     func removeRestaurantFromGroup(restaurantId: String, groupId: String) -> Single<Void>
+    func searchRestaurants(keyword: String, location: Location?) -> Single<[Restaurant]>
 }
 
 final class RestaurantService: BaseService, RestaurantServiceType {
@@ -24,5 +25,9 @@ final class RestaurantService: BaseService, RestaurantServiceType {
 
     func removeRestaurantFromGroup(restaurantId: String, groupId: String) -> Single<Void> {
         return .just(())
+    }
+
+    func searchRestaurants(keyword: String, location: Location?) -> Single<[Restaurant]> {
+        return .just(TestData.restaurants(count: 8))
     }
 }
