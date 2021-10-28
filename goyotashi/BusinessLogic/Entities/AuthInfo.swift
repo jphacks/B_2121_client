af914e80f036f2a0638a93198cb77aaaf415408b
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-29 01:57:08 +0900

    update AuthInfo

--- goyotashi/BusinessLogic/Entities/AuthInfo.swift
+++ goyotashi/BusinessLogic/Entities/AuthInfo.swift
@@ -5,7 +5,11 @@
 //  Created by Akihiro Kokubo on 2021/10/29.
 //
 
+import SwiftyUserDefaults
+
 struct AuthInfo {
     let token: String
     let vendor: String
 }
+
+extension AuthInfo: Codable, DefaultsSerializable {}
