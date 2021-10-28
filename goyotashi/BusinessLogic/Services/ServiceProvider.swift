1012074ac6d238d2289d9bab06de1cabef5b6f3e
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-28 12:28:34 +0900

    add services to ServiceProvider

--- goyotashi/BusinessLogic/Services/ServiceProvider.swift
+++ goyotashi/BusinessLogic/Services/ServiceProvider.swift
@@ -5,6 +5,16 @@
 //  Created by Akihiro Kokubo on 2021/10/28.
 //
 
-protocol ServiceProviderType: AnyObject {}
+protocol ServiceProviderType: AnyObject {
+    var userService: UserServiceType { get }
+    var restaurantService: RestaurantServiceType { get }
+    var groupService: GroupServiceType { get }
+    var bookmarkService: BookmarkServiceType { get }
+}
 
-final class ServiceProvider: ServiceProviderType {}
+final class ServiceProvider: ServiceProviderType {
+    lazy var userService: UserServiceType = UserService(provider: self)
+    lazy var restaurantService: RestaurantServiceType = RestaurantService(provider: self)
+    lazy var groupService: GroupServiceType = GroupService(provider: self)
+    lazy var bookmarkService: BookmarkServiceType = BookmarkService(provider: self)
+}
