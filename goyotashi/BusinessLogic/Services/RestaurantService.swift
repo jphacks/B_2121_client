af40c9cec3368d17a5b335d74b5396643c0397d4
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-28 11:30:45 +0900

    add RestaurantService

---
+++ goyotashi/BusinessLogic/Services/RestaurantService.swift
@@ -0,0 +1,14 @@
+//
+//  RestaurantService.swift
+//  goyotashi
+//
+//  Created by Akihiro Kokubo on 2021/10/28.
+//
+
+import RxSwift
+
+protocol RestaurantServiceType {
+}
+
+final class RestaurantService: BaseService, RestaurantServiceType {
+}
