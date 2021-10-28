5e65c25013c2ff3343247d58f8f19b20e94898be
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-28 11:55:06 +0900

    add BookmarkService

---
+++ goyotashi/BusinessLogic/Services/BookmarkService.swift
@@ -0,0 +1,14 @@
+//
+//  BookmarkService.swift
+//  goyotashi
+//
+//  Created by Akihiro Kokubo on 2021/10/28.
+//
+
+import RxSwift
+
+protocol BookmarkServiceType {
+}
+
+final class BookmarkService: BaseService, BookmarkServiceType {
+}
