dd23ad0419f572c009d4519633e871056bf6638b
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-28 11:52:17 +0900

    add searchGroup

--- goyotashi/BusinessLogic/Services/GroupService.swift
+++ goyotashi/BusinessLogic/Services/GroupService.swift
@@ -9,10 +9,15 @@ import RxSwift
 
 protocol GroupServiceType {
     func createGroup(group: Group) -> Single<Void>
+    func searchGroup(keyword: String, location: Location?) -> Single<[HomeGroup]>
 }
 
 final class GroupService: BaseService, GroupServiceType {
     func createGroup(group: Group) -> Single<Void> {
         return .just(())
+    }
+
+    func searchGroup(keyword: String, location: Location?) -> Single<[HomeGroup]> {
+        return .just(TestData.homeGroups(count: 9))
     }
 }
