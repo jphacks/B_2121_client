0f061bfdea73fe80223699e8c102ba4db2d53bc5
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-28 11:41:14 +0900

    add GroupService

---
+++ goyotashi/BusinessLogic/Services/GroupService.swift
@@ -0,0 +1,14 @@
+//
+//  GroupService.swift
+//  goyotashi
+//
+//  Created by Akihiro Kokubo on 2021/10/28.
+//
+
+import RxSwift
+
+protocol GroupServiceType {
+}
+
+final class GroupService: BaseService, GroupServiceType {
+}
