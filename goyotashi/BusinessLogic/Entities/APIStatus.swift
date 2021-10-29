07a60b9f37bbfad0bf6a347164865f81bd85d006
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-29 11:35:32 +0900

    add APIStatus

---
+++ goyotashi/BusinessLogic/Entities/APIStatus.swift
@@ -0,0 +1,14 @@
+//
+//  APIStatus.swift
+//  goyotashi
+//
+//  Created by Akihiro Kokubo on 2021/10/29.
+//
+
+enum APIStatus {
+    case pending
+    case refreshing
+    case loading
+    case succeeded
+    case failed
+}
