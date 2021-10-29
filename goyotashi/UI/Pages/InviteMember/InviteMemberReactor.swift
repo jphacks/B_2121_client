9ccfaea499166da2975c975503b7afde7c426313
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-30 07:31:00 +0900

    add invitationToken

--- goyotashi/UI/Pages/InviteMember/InviteMemberReactor.swift
+++ goyotashi/UI/Pages/InviteMember/InviteMemberReactor.swift
@@ -8,12 +8,17 @@
 import ReactorKit
 
 final class InviteMemberReactor: Reactor {
-    enum Action {}
-    enum Mutation {}
+    enum Action {
+        case tapInvitationLinkButton
+    }
+    enum Mutation {
+        case setInvitationToken(String)
+    }
 
     struct State {
         let members: [User] = TestData.users(count: 8)
         var memberCellReactors: [InviteMemberCellReactor] = (0 ..< 8).map { _ in InviteMemberCellReactor() }
+        var invitationToken: String = ""
     }
 
     let initialState: State
@@ -22,5 +27,21 @@ final class InviteMemberReactor: Reactor {
     init(provider: ServiceProviderType) {
         self.provider = provider
         initialState = State()
+    }
+
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case .tapInvitationLinkButton:
+            return .empty()
+        }
+    }
+
+    func reduce(state: State, mutation: Mutation) -> State {
+        var state = state
+        switch mutation {
+        case let .setInvitationToken(token):
+            state.invitationToken = token
+        }
+        return state
     }
 }
