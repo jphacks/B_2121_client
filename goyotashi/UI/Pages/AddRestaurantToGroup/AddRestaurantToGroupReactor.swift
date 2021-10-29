36e3539cef5864f2d1f2aeb32a7b76968fb7de4e
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-30 06:25:51 +0900

    add addRestaurant

--- goyotashi/UI/Pages/AddRestaurantToGroup/AddRestaurantToGroupReactor.swift
+++ goyotashi/UI/Pages/AddRestaurantToGroup/AddRestaurantToGroupReactor.swift
@@ -10,14 +10,17 @@ import ReactorKit
 final class AddRestaurantToGroupReactor: Reactor {
     enum Action {
         case refresh
+        case didItemSelect(IndexPath)
     }
     enum Mutation {
         case setGroupCellReactors([GroupSummary])
+        case setApiStatus(APIStatus)
     }
 
     struct State {
         var groupCellReactors: [AddRestaurantToGroupCellReactor] = []
         let restaurantId: Int64
+        var apiStatus: APIStatus = .pending
 
         init(restaurantId: Int64) {
             self.restaurantId = restaurantId
@@ -36,6 +39,19 @@ final class AddRestaurantToGroupReactor: Reactor {
         switch action {
         case .refresh:
             return refresh().map(Mutation.setGroupCellReactors)
+        case let .didItemSelect(indexPath):
+            if currentState.apiStatus != .pending { return .empty() }
+            let groupId = currentState.groupCellReactors[indexPath.row].currentState.groupSummary.groupId
+            return .concat(
+                .just(.setApiStatus(.loading)),
+                addRestaurant(groupId: groupId)
+                    .map { _ in .setApiStatus(.succeeded) }
+                    .catchError { error in
+                        logger.error(error)
+                        return .just(Mutation.setApiStatus(.failed))
+                    },
+                .just(.setApiStatus(.pending))
+            )
         }
     }
 
@@ -44,11 +60,18 @@ final class AddRestaurantToGroupReactor: Reactor {
         return provider.userService.getMyGroups(userId: userId).asObservable()
     }
 
+    private func addRestaurant(groupId: Int64) -> Observable<Void> {
+        return provider.restaurantService.addRestaurantToGroup(restaurantId: currentState.restaurantId, groupId: groupId)
+            .asObservable()
+    }
+
     func reduce(state: State, mutation: Mutation) -> State {
         var state = state
         switch mutation {
         case let .setGroupCellReactors(groupSummaries):
             state.groupCellReactors = groupSummaries.map { AddRestaurantToGroupCellReactor(groupSummary: $0) }
+        case let .setApiStatus(apiStatus):
+            state.apiStatus = apiStatus
         }
         return state
     }
