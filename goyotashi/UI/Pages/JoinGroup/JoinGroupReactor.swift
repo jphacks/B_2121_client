ac9b3e00153caf6d98925191639ceb3a858e3c25
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-30 10:22:49 +0900

    add updateInvitationCode

--- goyotashi/UI/Pages/JoinGroup/JoinGroupReactor.swift
+++ goyotashi/UI/Pages/JoinGroup/JoinGroupReactor.swift
@@ -8,12 +8,33 @@
 import ReactorKit
 
 final class JoinGroupReactor: Reactor {
-    enum Action {}
-    enum Mutation {}
+    enum Action {
+        case updateInvitationCode(String?)
+    }
+    enum Mutation {
+        case setInvitationCode(String)
+    }
 
     struct State {
-        let property: Int = 0
+        var invitationCode: String = ""
     }
 
     let initialState: State = State()
+
+    func mutate(action: Action) -> Observable<Mutation> {
+        switch action {
+        case let .updateInvitationCode(code):
+            guard let code = code else { return .empty() }
+            return .just(.setInvitationCode(code))
+        }
+    }
+
+    func reduce(state: State, mutation: Mutation) -> State {
+        var state = state
+        switch mutation {
+        case let .setInvitationCode(code):
+            state.invitationCode = code
+        }
+        return state
+    }
 }
