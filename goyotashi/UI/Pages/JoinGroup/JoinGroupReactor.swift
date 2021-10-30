6ee9033f6ba1046355b5c6a9051753df6ffd4445
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-30 10:29:49 +0900

    add join

--- goyotashi/UI/Pages/JoinGroup/JoinGroupReactor.swift
+++ goyotashi/UI/Pages/JoinGroup/JoinGroupReactor.swift
@@ -10,13 +10,16 @@ import ReactorKit
 final class JoinGroupReactor: Reactor {
     enum Action {
         case updateInvitationCode(String?)
+        case join
     }
     enum Mutation {
         case setInvitationCode(String)
+        case setApiStatus(APIStatus)
     }
 
     struct State {
         var invitationCode: String = ""
+        var apiStatus: APIStatus = .pending
     }
 
     let initialState: State = State()
@@ -26,7 +29,18 @@ final class JoinGroupReactor: Reactor {
         case let .updateInvitationCode(code):
             guard let code = code else { return .empty() }
             return .just(.setInvitationCode(code))
+        case .join:
+            if currentState.apiStatus != .pending { return .empty() }
+            return .concat(
+                .just(.setApiStatus(.loading)),
+                join().map { _ in .setApiStatus(.succeeded) },
+                .just(.setApiStatus(.pending))
+            )
         }
+    }
+
+    private func join() -> Observable<Void> {
+        return .empty()
     }
 
     func reduce(state: State, mutation: Mutation) -> State {
@@ -34,6 +48,9 @@ final class JoinGroupReactor: Reactor {
         switch mutation {
         case let .setInvitationCode(code):
             state.invitationCode = code
+            logger.debug(code)
+        case let .setApiStatus(apiStatus):
+            state.apiStatus = apiStatus
         }
         return state
     }
