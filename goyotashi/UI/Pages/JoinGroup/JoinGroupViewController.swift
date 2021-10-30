366bf16f38d930d8748568a61e61dc776c23c48a
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-30 09:49:09 +0900

    add JoinGroupViewController and reactor

---
+++ goyotashi/UI/Pages/JoinGroup/JoinGroupViewController.swift
@@ -0,0 +1,41 @@
+//
+//  JoinGroupViewController.swift
+//  goyotashi
+//
+//  Created by Akihiro Kokubo on 2021/10/30.
+//
+
+import UIKit
+import ReactorKit
+
+final class JoinGroupViewController: UIViewController, View, ViewConstructor {
+
+    // MARK: - Variables
+    var disposeBag = DisposeBag()
+
+    // MARK: - Views
+
+    // MARK: - Lify Cycles
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupViews()
+        setupViewConstraints()
+    }
+
+    // MARK: - Setup Methods
+    func setupViews() {
+
+    }
+
+    func setupViewConstraints() {
+
+    }
+
+    // MARK: - Bind Method
+    func bind(reactor: JoinGroupReactor) {
+        // Action
+
+        // State
+    }
+}
