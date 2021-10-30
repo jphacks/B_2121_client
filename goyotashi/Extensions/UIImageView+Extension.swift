95170fa65f7bc12176066842e6964c749a9cc591
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-30 21:34:03 +0900

    add HPGImageSizeType

--- goyotashi/Extensions/UIImageView+Extension.swift
+++ goyotashi/Extensions/UIImageView+Extension.swift
@@ -9,16 +9,16 @@ import UIKit
 import Kingfisher
 
 extension UIImageView {
-    func setImageWithHPGUrl(urlString: String) {
-        let updatedUrlString = updateSuffix(urlString: urlString)
+    func setImageWithHPGUrl(urlString: String, sizeType: HPGImageSizeType = .large) {
+        let updatedUrlString = updateSuffix(urlString: urlString, sizeType: sizeType)
         let sources: [Source] = [urlString].compactMap(buildSource(urlString:))
         let optionsInfo = KingfisherOptionsInfoItem.alternativeSources(sources)
         self.kf.setImage(with: URL(string: updatedUrlString), placeholder: R.image.dish(), options: [optionsInfo])
     }
 
-    private func updateSuffix(urlString: String) -> String {
+    private func updateSuffix(urlString: String, sizeType: HPGImageSizeType) -> String {
         let prefix = String(urlString.prefix(urlString.count - 7))
-        let updatedUrlString = "\(prefix)480.jpg"
+        let updatedUrlString = "\(prefix)\(sizeType.rawValue).jpg"
         return updatedUrlString
     }
 
@@ -28,4 +28,10 @@ extension UIImageView {
         let source = Source.network(url)
         return source
     }
+}
+
+enum HPGImageSizeType: Int {
+    case large = 480
+    case medium = 238
+    case small = 168
 }
