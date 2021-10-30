7180a06eb9ae169318912e508b4778febd59a67c
Akihiro Kokubo <akihiro26melle08cielo@gmail.com>
2021-10-30 11:12:05 +0900

    add userEventAction

--- goyotashi/UI/Pages/Profile/ProfileGroupList/ProfileGroupListReactor.swift
+++ goyotashi/UI/Pages/Profile/ProfileGroupList/ProfileGroupListReactor.swift
@@ -54,10 +54,20 @@ final class ProfileGroupListReactor: Reactor {
                     }
                 }
             }
+        let userEventAction = provider.userService.event
+            .flatMap { event -> Observable<Action> in
+                switch event {
+                case .didUpdateUser:
+                    return .empty()
+                case .didJoinGroup:
+                    return .just(.refresh)
+                }
+            }
         return .merge(
             action,
             groupEventAction,
-            bookmarkEventAction
+            bookmarkEventAction,
+            userEventAction
         )
     }
 
